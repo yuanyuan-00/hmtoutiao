d47385e63397b54cdf7fdf3a0688a0ae136fa9a7
yuanyuan-00 <1998044+yuanyuan0000@user.noreply.gitee.com>
2019-11-12 09:43:58 +0800

    login test commit

--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,14 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 
+import Login from './../views/login'
+
 Vue.use(VueRouter)
 
 const routes = [
-
+  // 重定向
+  { path: '/', redirect: '/login' },
+  { path: '/login', component: Login }
 ]
 
 const router = new VueRouter({
