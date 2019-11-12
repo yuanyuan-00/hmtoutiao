9ce405f2c40aedd91d374c1b82b87230685fa9ba
yuanyuan-00 <1998044+yuanyuan0000@user.noreply.gitee.com>
2019-11-12 19:30:20 +0800

    第一次提交

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,13 +1,63 @@
 <template>
-  <div>
-    登录页面
+  <div id="app">
+    <div id="login-box">
+      <el-form
+        ref="LoginForm"
+        :model="loginForm"
+        :rules="loginformrules"
+      >
+        <el-form-item prop="mobile">
+          <el-input placeholder="请输入手机号码" v-model="loginForm.mobile"></el-input>
+        </el-form-item>
+        <el-form-item prop="code">
+          <el-input placeholder="请输入校验码" v-model="loginForm.code"></el-input>
+        </el-form-item>
+        <el-form-item>
+          <el-checkbox v-model="loginForm.xieyi"></el-checkbox>
+          <span>我同意当前协议</span>
+        </el-form-item>
+        <el-form-item>
+          <el-button type="primary" style="width:100%;">登录</el-button>
+        </el-form-item>
+      </el-form>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
+  data () {
+    return {
+      LoginForm: {
+        mobile: '',
+        code: '',
+        xieyi: false
+      },
+      loginformrules: {
+        mobile: [
+          { required: true, message: '请输入正确的手机号码', trigger: 'blur' }
+        ],
+        code: [
+          { required: true, message: '请输入正确的验证码', trigger: 'blur' }
+        ]
+      }
+    }
+  }
 }
 </script>
 
 <style lang="less" scoped>
+#app {
+  background-color: green;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  #login-box {
+    width: 400px;
+    height: 300px;
+    background-color: #ccc;
+    box-sizing: border-box;
+    padding: 30px;
+  }
+}
 </style>
